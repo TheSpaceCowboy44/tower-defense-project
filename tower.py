3148ddf65e295914deddd0dccf3f24eaf321a870
Aymeric Potier <aymeric.potier@visit.com>
2024-01-31 17:00:38 +0100

    started implementing tower shooting

--- tower.py
+++ tower.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 from pygame.locals import *
 
@@ -14,3 +15,20 @@ class Tower(pygame.sprite.Sprite):
             circle_radius = min(self.rect.width, self.rect.height) // 2
             circle_center = (self.rect.centerx, self.rect.centery)
             pygame.draw.circle(surface, GREEN, circle_center, circle_radius, 2)
+    def shoot(self):
+        position = pygame.math.Vector2(3, 4)
+        velocity = pygame.math.Vector2(1, 0)
+        position += velocity
+        magnitude = position.magnitude()
+        normalized_vector = position.normalize()
+        #NEED TO UNDERSTAND how it works
+
+        bullet = TowerBullet(self)
+
+class TowerBullet(pygame.sprite.Sprite):
+    def __init__(self, tower):
+        super().__init__()
+        self.rect = pygame.Rect(tower.rect.x, tower.rect.y, 1, 4)
+        self.angle = random.randrange(0,359)
+    def draw(self, surface):
+        pygame.draw.ellipse(surface, ORANGE_LIGHT, self.rect, width=0)
