ea8b38f851c6da7ef76421b6727a5f2bbdff3067
Aymeric Potier <aymeric.potier@visit.com>
2024-02-16 15:29:24 +0100

    still working on this rotation issue :(

--- fake_block.py
+++ fake_block.py
@@ -1,24 +1,22 @@
 import pygame
 from pygame.locals import *
 from constants import YELLOW_2
-from utils import blitRotateCenter
+from utils import *
 
 class FakeBlock(pygame.sprite.Sprite):
     def __init__(self, angle):
         super().__init__()
-        self.original_image = pygame.Surface((2, 8))
-        self.original_image.fill(YELLOW_2)
-        self.image = self.original_image
+        self.image = pygame.Surface((2, 8))
+        self.image.fill(YELLOW_2)
         self.rect = self.image.get_rect(center=(50, 50)) 
         self.angle = angle
 
     def update(self):
         self.angle += 1
         self.angle %= 360  
-        new_image = pygame.transform.rotate(self.original_image, -self.angle)
-        old_center = self.rect.center 
-        self.image = new_image
-        self.rect = self.image.get_rect(center=old_center) 
 
     def draw(self, surface):
         surface.blit(self.image, self.rect.topleft)
+        w, h = self.image.get_size()
+        pos = (self.rect.centerx, self.rect.centery)
+        blitRotate(surface, self.image, pos, (w/2, h/2), self.angle)
--- utils.py
+++ utils.py
@@ -51,7 +51,11 @@ def calculer_angle(x1, y1, x2, y2):
 
     return angle_degrees
 
-def blitRotateCenter(surf, image, topleft, angle):
+def blitRotate(surf, image, pos, originPos, angle):
+    image_rect = image.get_rect(topleft=(pos[0] - originPos[0], pos[1] - originPos[1]))
+    offset_center_to_pivot = pygame.math.Vector2(pos) - image_rect.center
+    rotated_offset = offset_center_to_pivot.rotate(-angle)
+    rotated_image_center = (pos[0] - rotated_offset.x, pos[1] - rotated_offset.y)
     rotated_image = pygame.transform.rotate(image, angle)
-    new_rect = rotated_image.get_rect(center = image.get_rect(topleft = topleft).center)
-    surf.blit(rotated_image, new_rect)
+    rotated_image_rect = rotated_image.get_rect(center=rotated_image_center)
+    surf.blit(rotated_image, rotated_image_rect.topleft)
