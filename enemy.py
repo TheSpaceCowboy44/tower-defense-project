c1c3b324c90e408d9b4a9ef94ea7316aba3064cc
Aymeric Potier <aymeric.potier@visit.com>
2024-09-10 15:06:35 +0200

    added debug display

--- enemy.py
+++ enemy.py
@@ -17,55 +17,54 @@ class Enemy(pygame.sprite.Sprite):
     def update(self):
         for i, routeStep in enumerate(self.route):
             if(routeStep.is_active):
-
                 if(routeStep.direction == Direction.DOWN):
                     if(routeStep.typeOfStep == TypeOfStep.EXIT):
                         self.rect.y = (self.rect.y + self.speed)
                     else:
-                        if(self.rect.y <= routeStep.position.y):
+                        if(self.rect.y <= routeStep.center_y):
                             self.rect.y = (self.rect.y + self.speed)
-                        elif(self.rect.y > routeStep.position.y):
-                            self.checkRoute(i)
+                        elif(self.rect.y > routeStep.center_y):
+                            self.checkRoute(i, i+6)
+                            displayCurrentRouteSteps(self.route)
                             break
                 if(routeStep.direction == Direction.UP):
                     if(routeStep.typeOfStep == TypeOfStep.EXIT):
                         self.rect.y = (self.rect.y - self.speed)
                     else:
-                        if (self.rect.y >= routeStep.position.y):
+                        if (self.rect.y >= routeStep.center_y):
                             self.rect.y = (self.rect.y - self.speed)
-                        elif (self.rect.y < routeStep.position.y):
-                            self.checkRoute(i)
+                        elif (self.rect.y < routeStep.center_y):
+                            self.checkRoute(i, i-6)
+                            displayCurrentRouteSteps(self.route)
                             break
                 if(routeStep.direction == Direction.RIGHT):
-                    json_data = json.dumps([step.to_dict() for step in self.route], indent=4)
-                    print("###########################################################################################################")
-                    print("###########################################################################################################")
-                    print("###########################################################################################################")
-                    print("###########################################################################################################")
-                    print(json_data)
                     if(routeStep.typeOfStep == TypeOfStep.EXIT):
                         self.rect.x = (self.rect.x + self.speed)
                     else:
-                        if (self.rect.x <= routeStep.position.x):
+                        if (self.rect.x <= routeStep.center_x):
                             self.rect.x = (self.rect.x + self.speed)
-                        elif (self.rect.x > routeStep.position.x):
-                            self.checkRoute(i)
+                        elif (self.rect.x > routeStep.center_x):
+                            self.checkRoute(i, i+1)
+                            displayCurrentRouteSteps(self.route)
                             break
                 if(routeStep.direction == Direction.LEFT):
                     if(routeStep.typeOfStep == TypeOfStep.EXIT):
                         self.rect.x = (self.rect.x - self.speed)
                     else:
-                        if(self.rect.x >= routeStep.position.x):
+                        if(self.rect.x >= routeStep.center_x):
                             self.rect.x = (self.rect.x - self.speed)
-                        elif (self.rect.x < routeStep.position.x):
-                            self.checkRoute(i)
+                        elif (self.rect.x < routeStep.center_x):
+                            self.checkRoute(i, i-1)
+                            displayCurrentRouteSteps(self.route)
                             break
     def draw(self, surface):
         surface.blit(self.image, self.rect.topleft)
-    def checkRoute(self, i):
-        self.route[i].is_active = False
-        if i + 1 < len(self.route) and self.route[i+1] is not None:
-            self.route[i+1].is_active = True
+    def checkRoute(self, i, nextStepIndex):
+        if self.route[i].typeOfStep != TypeOfStep.EXIT:
+            self.route[i].is_active = False
+            if(nextStepIndex is not None):
+                self.route[nextStepIndex].is_active = True
+
 
 class Enemy_Type_1(Enemy):
     def __init__(self, spawn_x, spawn_y):
@@ -111,6 +110,7 @@ class Enemy_Type_3(Enemy):
         self.damage = ENEMY_TYPE_3_DAMAGE
     def draw(self, surface):
         surface.blit(self.image, self.rect.topleft)
+        
 
 class EnemyRouteStep():
     def __init__(self, position, direction, is_active, typeOfStep):
@@ -118,6 +118,8 @@ class EnemyRouteStep():
         self.direction = direction
         self.is_active = is_active
         self.typeOfStep = typeOfStep
+        self.center_x = position.x + BLOCK_SIZE/2
+        self.center_y = position.y + BLOCK_SIZE/2
     def to_dict(self):
         return {
             "pos": f"{self.position.x},{self.position.y}",
@@ -127,3 +129,27 @@ class EnemyRouteStep():
         }
 
 
+def displayCurrentRouteSteps(steps):
+    for step in steps:
+        if(steps.index(step) % 6):
+            print("\n")
+        if(step.direction == Direction.RIGHT):
+            if(step.is_active):
+                print("→")
+            else:
+                print("➔")
+        elif(step.direction == Direction.LEFT):
+            if(step.is_active):
+                print("←")
+            else:
+                print("🠔")
+        elif(step.direction == Direction.UP):
+            if(step.is_active):
+                print("↑")
+            else:
+                print("🠕")
+        elif(step.direction == Direction.DOWN):
+            if(step.is_active):
+                print("↓")
+            else:
+                print("🠗")
